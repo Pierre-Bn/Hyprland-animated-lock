3e936398f763292c08cb6acae450104c1ce841ad
Estix <estix109@gmail.com>
2024-12-17 18:06:30 +0100

    Now with support for multiple monitors

--- Hyprlock-animated.sh
+++ Hyprlock-animated.sh
@@ -8,14 +8,32 @@ FILE_NAME=output.webm
 #Also default path to the utils directory since that's where the file generates
 FILE_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/Hyprlock-animated/utils/$FILE_NAME"
 
-# Start mpv in a loop
-mpv --fullscreen --no-audio --loop --no-osc "$FILE_PATH" &
+# Function to start mpv on each monitor
+start_mpv_on_all_monitors() {
+    MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
 
-# Get mpv process ID
-MPV_PID=$!
+    for MONITOR in $MONITORS; do
+        # Start mpv on the current monitor
+        hyprctl dispatch -- exec "[monitor $MONITOR]" mpv --fullscreen --no-audio --loop --no-osc "$FILE_PATH" &
+        MPV_PIDS+=($!)  # Collect the process IDs of all mpv instances
+    done
+}
+
+# Array to store mpv process IDs
+MPV_PIDS=()
+
+# Start mpv on all monitors
+start_mpv_on_all_monitors
 
 # Run Hyprlock
 hyprlock
+# Run Swaylock
+#swaylock -c 00000022
 
-# Once Hyprlock exits, stop VLC
-kill $MPV_PID
+
+# Once Hyprlock exits, stop all mpv instances
+MPV_PIDS=$(ps -aux | grep "mpv --fullscreen" | awk '{print $2}')
+for PID in $MPV_PIDS; do
+    kill "$PID"
+done
+
