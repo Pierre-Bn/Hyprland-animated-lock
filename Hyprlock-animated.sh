a1f862d4d7a57a27645b9b0284a12973c01b34e8
Estix <estix109@gmail.com>
2024-12-17 18:35:25 +0100

    Now without killing all other mpv instances

--- Hyprlock-animated.sh
+++ Hyprlock-animated.sh
@@ -15,12 +15,8 @@ start_mpv_on_all_monitors() {
     for MONITOR in $MONITORS; do
         # Start mpv on the current monitor
         hyprctl dispatch -- exec "[monitor $MONITOR]" mpv --fullscreen --no-audio --loop --no-osc "$FILE_PATH" &
-        MPV_PIDS+=($!)  # Collect the process IDs of all mpv instances
     done
 }
-
-# Array to store mpv process IDs
-MPV_PIDS=()
 
 # Start mpv on all monitors
 start_mpv_on_all_monitors
@@ -30,10 +26,4 @@ hyprlock
 # Run Swaylock
 #swaylock -c 00000022
 
-
-# Once Hyprlock exits, stop all mpv instances
-MPV_PIDS=$(ps -aux | grep "mpv --fullscreen" | awk '{print $2}')
-for PID in $MPV_PIDS; do
-    kill "$PID"
-done
-
+pkill -f "mpv --fullscreen --no-audio --loop --no-osc $FILE_PATH"
